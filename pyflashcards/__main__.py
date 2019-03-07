e723144b7679b19d2fde6a85c35a24af4b4d8b10
Chris Luedtke <chrisluedtke@gmail.com>
2019-03-07 14:37:14 -0600

    consolidate into quiz module

--- pyflashcards/__main__.py
+++ pyflashcards/__main__.py
@@ -1,7 +1,3 @@
 from quiz import quiz
-from cards_pandas import cards_pandas
-from cards_effective_python import cards_effective_python
 
-cards = cards_effective_python + cards_pandas
-
-quiz(cards)
+quiz()
