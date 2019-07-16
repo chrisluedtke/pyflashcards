35d9c8664267a4b27660653442fb6e7ac3538bce
Chris Luedtke <chrisluedtke@gmail.com>
2019-07-16 12:14:48 -0500

    Update config.py

--- pyflashcards/config.py
+++ pyflashcards/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 class Config(object):
     SECRET_KEY = getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = getenv('FLASK_ENV')
